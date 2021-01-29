04d95bebfeac013e68a3cab2b1f1a6b4d5091671
stmar89 <stefano.marseglia89@gmail.com>
2021-01-29 09:02:41 -0500

    cleaned up the code from the old intrinsinc and removed some tests

--- ResRefCond.m
+++ ResRefCond.m
@@ -358,150 +358,153 @@ intrinsic ResidualReflexCondition(AVh::IsogenyClassFq : MinPrecision:=30) -> Seq
     return AVh`RRC_CMTypes;
 end intrinsic;
 
-// TO BE REMOVED
-intrinsic CCO_OLD(AVh::IsogenyClassFq : MinPrecision:=30) -> SeqEnum[AlgAssCMType]
-{   
-    It returns the sequence of CMTypes of the isogeny class AVh that satisfy the Residue Reflex Condition (RRC). 
-    MinPrecision is the minimum precision to construct the p-adic splitting field (see below).
-
-    Recall that a CMType PHI satisfies RRC if: 
-        *) the CM-type satisfies the Shimura-Taniyama formula, and
-        *) the associated reflex field has residue field that can be realized as a subfield of the field of definition of AVh.
-    We build create Q and Qp-splitting field of the Weil polynomil and hence a bijection between complex and p-adic roots. 
-    This allow us to do the tests in the p-adic splitting field, increasing speed.
-    The intermediate data is recorded in the attribute RRC_data. See above for a detailed description.
-}
-    vprintf ResRefCond : "CCO_OLD\n";
-        q:=FiniteField(AVh);
-        all_cm:=AllCMTypes(AVh);
-        bs:=[ CMPosElt(PHI) : PHI in all_cm ];
-        prec:=MinPrecision;
-
-        ////////////////----bijection between p-adic and CC roots via SplittingFields----///////////////////
-        L:=Parent(bs[1]);
-        h:=DefiningPolynomial(L);
-        FL:=PrimitiveElement(L);
-        _,p:=IsPrimePower(q);
-        M,rtsM:=SplittingField(h);
-        prec:=Max([prec] cat [Valuation(c,p) : c in Coefficients(h) | c ne 0]);        
-        Zp:=pAdicRing(p,prec);
-        hp:=ChangeRing(h,Zp);
-        prec:=Max([prec,2*SuggestedPrecision(hp),2*SuggestedPrecision(ChangeRing(DefiningPolynomial(M),Zp))]);
-        ChangePrecision(~Zp,prec);
-        hp:=ChangeRing(h,Zp);
-        repeat
-            go:=true;
-            try
-                np:=DefiningPolynomial(SplittingField(hp),Zp);
-            catch e
-                prec +:= 50;
-                ChangePrecision(~Zp,prec);
-                hp:=ChangeRing(h,Zp);
-                go:=false;
-                vprintf ResRefCond : "precision increased to %o\n",prec;
-            end try;
-        until go;
-
-        fac:=[ g[1] : g in Factorization(hp) ];
-        N:=LocalField(FieldOfFractions(Zp),np);
-        rootM_inN:=Roots(DefiningPolynomial(M),N)[1,1];          // sometimes the precision is not enough ?
-        /* 
-            // alternative to force higher precision. I don't like it.
-            N0:=LocalField(FieldOfFractions(ChangePrecision(Zp,2*prec)),np); // this is defined to force a higher precision 
-                                                                             // in the computation of eps below...
-                                                                             // it is weird and I don't like it.
-            rootM_inN:=N!Eltseq(Roots(DefiningPolynomial(M),N0)[1,1]);
-            // this aproach seems to create problems later in the line
-            // E:=sub< N | gens_E >; //sometimes it seems to crash...
-            // for poly like:
-            // x^6 - x^5 + 2*x^3 - 4*x + 8, x^6 - x^5 + 4*x^4 - 2*x^3 + 8*x^2 - 4*x + 8, x^6 + x^5 - 2*x^3 + 4*x + 8
-            // hence we remove it
-        */
-        eps:=hom<M->N | rootM_inN >; // a choice of M->N. 
-                                     // exists because both M and N are splitting fields 
-                                     
-        vprint ResRefCond : Evaluate(DefiningPolynomial(M),eps(M.1));
-        assert IsWeaklyZero(Evaluate(DefiningPolynomial(M),eps(M.1)));
-        all_resrefl:=[];
-        all_st:=[];
-        facq:=Factorization(q*MaximalOrder(L));
-        primes:=[ P[1] : P in facq ];
-        valsq:=[ P[2] : P in facq ];
-        facFL:=Factorization(FL*MaximalOrder(L));
-        valsFL:=[  ];
-        hp_fac:=[ ];
-        RHS_D_P:=[ ];
-        for P in primes do
-            vFLP:=[ fac[2] : fac in facFL | fac[1] eq P ];
-            assert #vFLP in {0,1};
-            vFLP:= (#vFLP eq 1) select vFLP[1] else 0;
-            Append(~valsFL,vFLP);
-            LP,mLP:=Completion(P : MinPrecision:=prec );
-            Pfac:=[ gp : gp in fac | Valuation(Evaluate(gp,mLP(FL))) gt (prec div 2) ];
-            assert #Pfac eq 1;
-            Append(~hp_fac,Pfac[1]);
-            RHS_D:=#[ r : r in rtsM | Valuation(Evaluate(Pfac[1],eps(r))) gt (prec div 2) ];
-            assert RHS_D eq Degree(Pfac[1]);
-            Append(~RHS_D_P,RHS_D);
-        end for;
-        pow_bas_L:=[FL^(i-1) : i in [1..Degree(h)]];
-        refl_fields:=[];
-
-        // (early exit on N)
-        // Denote the residue field of N by kN. The residue field of any subfield of N is a subfield of kN.
-        // Hence, if kN is a subfield of Fq=FiniteField(AVh) then the same is true for the residue fields of
-        // the reflex fields.
-        // If this happens, we set the marker compute_reflex_fields:=false and skip the computation of the reflex fields 
-        // which is the bottleneck of function. In particular refl_fields will be left empty
-        if (Ilog(p,q)) mod Ilog(p,#ResidueClassField(N)) eq 0 then
-            compute_reflex_fields:=false;
-            all_resrefl:=[ true : i in [1..#bs] ];
-        else 
-            compute_reflex_fields:=true;
-        end if;
-        vprint ResRefCond : "early exit on N",compute_reflex_fields;
-
-        // now we loop over all cm-types
-        for b in bs do
-            assert2 b eq &+[(Coordinates([b],pow_bas_L)[1,i])*FL^(i-1) : i in [1..Degree(h)]];
-            rtsM_PHI:=[];
-            for FM in rtsM do
-                bM:=&+[(Coordinates([b],pow_bas_L)[1,i])*FM^(i-1) : i in [1..Degree(h)]]; // bM = 'image' of b in M 
-                assert2 bM eq -ComplexConjugate(bM);
-                if Im(Conjugates(bM)[1]) gt 0 then  // this is the choice phi_0:M->CC
-                                                    // which induces a bijection Hom(L,C) <-> rtsM given by
-                                                    // phi |-> the unique pi_j such that phi_0(pi_j)=phi(pi)
-                    Append(~rtsM_PHI,FM);
-                end if;
-                // write b=sum b_k pi^(k-t)
-                // phi in PHI iff Im(phi(b))>0 iff Im(sum b_k phi(pi)^(k-1)) >0 iff Im(sum b_k phi_0(pi_j))>0 iff 
-                //            iff bM=sum b_k phi_0(pi_j) in rtsM_PHI. 
-            end for;
-            assert #rtsM_PHI eq Degree(h) div 2;
-            ////////////////----residue field of reflex field, pAdic ----///////////////////
-            if compute_reflex_fields then //check early exit on N: see above.
-                h_fac:=[ hi[1] : hi in Factorization(h)];
-                gens_E_inM:=&cat[[ &+[ (r)^i : r in rtsM_PHI | Evaluate(hi,r) eq 0 ] : i in [0..Degree(hi)-1] ] : hi in h_fac];
-                gens_E:=[ eps(g) : g in gens_E_inM ];
-                E:=sub< N | gens_E >; //sometimes it seems to crash...
-                resrefl:=(Ilog(p,q)) mod Ilog(p,#ResidueClassField(E))  eq 0;
-                Append(~refl_fields,E);
-                Append(~all_resrefl,resrefl);
-            end if;
-            ////////////////----Shimura-Taniyama----///////////////////
-            st_tests:=[];
-            for iP in [1..#primes] do
-                RHS_N:=#[ r : r in rtsM_PHI | Valuation(Evaluate(hp_fac[iP],eps(r))) gt (prec div 2) ];
-                LHS:=valsFL[iP]/valsq[iP];
-                RHS:=RHS_N/RHS_D_P[iP];
-                Append(~st_tests, LHS eq RHS );
-            end for;
-            st:=&and(st_tests);
-            Append(~all_st,st);
-        end for;
-        RRC_CMTypes:=[ all_cm[i] : i in [1..#all_cm] | all_resrefl[i] and all_st[i] ];
-    return RRC_CMTypes;
-end intrinsic;
+// // TO BE REMOVED, eventually. This intrinsic is superseeded by the above one ResidualReflexCondition, where we have a 
+//                               much better control of the Precision (avoiding several bugs). The timings are very similar.
+//                               The old version is kept for consultation.
+//
+// intrinsic CCO_OLD(AVh::IsogenyClassFq : MinPrecision:=30) -> SeqEnum[AlgAssCMType]
+// {   
+//     It returns the sequence of CMTypes of the isogeny class AVh that satisfy the Residue Reflex Condition (RRC). 
+//     MinPrecision is the minimum precision to construct the p-adic splitting field (see below).
+// 
+//     Recall that a CMType PHI satisfies RRC if: 
+//         *) the CM-type satisfies the Shimura-Taniyama formula, and
+//         *) the associated reflex field has residue field that can be realized as a subfield of the field of definition of AVh.
+//     We build create Q and Qp-splitting field of the Weil polynomil and hence a bijection between complex and p-adic roots. 
+//     This allow us to do the tests in the p-adic splitting field, increasing speed.
+//     The intermediate data is recorded in the attribute RRC_data. See above for a detailed description.
+// }
+//     vprintf ResRefCond : "CCO_OLD\n";
+//         q:=FiniteField(AVh);
+//         all_cm:=AllCMTypes(AVh);
+//         bs:=[ CMPosElt(PHI) : PHI in all_cm ];
+//         prec:=MinPrecision;
+// 
+//         ////////////////----bijection between p-adic and CC roots via SplittingFields----///////////////////
+//         L:=Parent(bs[1]);
+//         h:=DefiningPolynomial(L);
+//         FL:=PrimitiveElement(L);
+//         _,p:=IsPrimePower(q);
+//         M,rtsM:=SplittingField(h);
+//         prec:=Max([prec] cat [Valuation(c,p) : c in Coefficients(h) | c ne 0]);        
+//         Zp:=pAdicRing(p,prec);
+//         hp:=ChangeRing(h,Zp);
+//         prec:=Max([prec,2*SuggestedPrecision(hp),2*SuggestedPrecision(ChangeRing(DefiningPolynomial(M),Zp))]);
+//         ChangePrecision(~Zp,prec);
+//         hp:=ChangeRing(h,Zp);
+//         repeat
+//             go:=true;
+//             try
+//                 np:=DefiningPolynomial(SplittingField(hp),Zp);
+//             catch e
+//                 prec +:= 50;
+//                 ChangePrecision(~Zp,prec);
+//                 hp:=ChangeRing(h,Zp);
+//                 go:=false;
+//                 vprintf ResRefCond : "precision increased to %o\n",prec;
+//             end try;
+//         until go;
+// 
+//         fac:=[ g[1] : g in Factorization(hp) ];
+//         N:=LocalField(FieldOfFractions(Zp),np);
+//         rootM_inN:=Roots(DefiningPolynomial(M),N)[1,1];          // sometimes the precision is not enough ?
+//         /* 
+//             // alternative to force higher precision. I don't like it.
+//             N0:=LocalField(FieldOfFractions(ChangePrecision(Zp,2*prec)),np); // this is defined to force a higher precision 
+//                                                                              // in the computation of eps below...
+//                                                                              // it is weird and I don't like it.
+//             rootM_inN:=N!Eltseq(Roots(DefiningPolynomial(M),N0)[1,1]);
+//             // this aproach seems to create problems later in the line
+//             // E:=sub< N | gens_E >; //sometimes it seems to crash...
+//             // for poly like:
+//             // x^6 - x^5 + 2*x^3 - 4*x + 8, x^6 - x^5 + 4*x^4 - 2*x^3 + 8*x^2 - 4*x + 8, x^6 + x^5 - 2*x^3 + 4*x + 8
+//             // hence we remove it
+//         */
+//         eps:=hom<M->N | rootM_inN >; // a choice of M->N. 
+//                                      // exists because both M and N are splitting fields 
+//                                      
+//         vprint ResRefCond : Evaluate(DefiningPolynomial(M),eps(M.1));
+//         assert IsWeaklyZero(Evaluate(DefiningPolynomial(M),eps(M.1)));
+//         all_resrefl:=[];
+//         all_st:=[];
+//         facq:=Factorization(q*MaximalOrder(L));
+//         primes:=[ P[1] : P in facq ];
+//         valsq:=[ P[2] : P in facq ];
+//         facFL:=Factorization(FL*MaximalOrder(L));
+//         valsFL:=[  ];
+//         hp_fac:=[ ];
+//         RHS_D_P:=[ ];
+//         for P in primes do
+//             vFLP:=[ fac[2] : fac in facFL | fac[1] eq P ];
+//             assert #vFLP in {0,1};
+//             vFLP:= (#vFLP eq 1) select vFLP[1] else 0;
+//             Append(~valsFL,vFLP);
+//             LP,mLP:=Completion(P : MinPrecision:=prec );
+//             Pfac:=[ gp : gp in fac | Valuation(Evaluate(gp,mLP(FL))) gt (prec div 2) ];
+//             assert #Pfac eq 1;
+//             Append(~hp_fac,Pfac[1]);
+//             RHS_D:=#[ r : r in rtsM | Valuation(Evaluate(Pfac[1],eps(r))) gt (prec div 2) ];
+//             assert RHS_D eq Degree(Pfac[1]);
+//             Append(~RHS_D_P,RHS_D);
+//         end for;
+//         pow_bas_L:=[FL^(i-1) : i in [1..Degree(h)]];
+//         refl_fields:=[];
+// 
+//         // (early exit on N)
+//         // Denote the residue field of N by kN. The residue field of any subfield of N is a subfield of kN.
+//         // Hence, if kN is a subfield of Fq=FiniteField(AVh) then the same is true for the residue fields of
+//         // the reflex fields.
+//         // If this happens, we set the marker compute_reflex_fields:=false and skip the computation of the reflex fields 
+//         // which is the bottleneck of function. In particular refl_fields will be left empty
+//         if (Ilog(p,q)) mod Ilog(p,#ResidueClassField(N)) eq 0 then
+//             compute_reflex_fields:=false;
+//             all_resrefl:=[ true : i in [1..#bs] ];
+//         else 
+//             compute_reflex_fields:=true;
+//         end if;
+//         vprint ResRefCond : "early exit on N",compute_reflex_fields;
+// 
+//         // now we loop over all cm-types
+//         for b in bs do
+//             assert2 b eq &+[(Coordinates([b],pow_bas_L)[1,i])*FL^(i-1) : i in [1..Degree(h)]];
+//             rtsM_PHI:=[];
+//             for FM in rtsM do
+//                 bM:=&+[(Coordinates([b],pow_bas_L)[1,i])*FM^(i-1) : i in [1..Degree(h)]]; // bM = 'image' of b in M 
+//                 assert2 bM eq -ComplexConjugate(bM);
+//                 if Im(Conjugates(bM)[1]) gt 0 then  // this is the choice phi_0:M->CC
+//                                                     // which induces a bijection Hom(L,C) <-> rtsM given by
+//                                                     // phi |-> the unique pi_j such that phi_0(pi_j)=phi(pi)
+//                     Append(~rtsM_PHI,FM);
+//                 end if;
+//                 // write b=sum b_k pi^(k-t)
+//                 // phi in PHI iff Im(phi(b))>0 iff Im(sum b_k phi(pi)^(k-1)) >0 iff Im(sum b_k phi_0(pi_j))>0 iff 
+//                 //            iff bM=sum b_k phi_0(pi_j) in rtsM_PHI. 
+//             end for;
+//             assert #rtsM_PHI eq Degree(h) div 2;
+//             ////////////////----residue field of reflex field, pAdic ----///////////////////
+//             if compute_reflex_fields then //check early exit on N: see above.
+//                 h_fac:=[ hi[1] : hi in Factorization(h)];
+//                 gens_E_inM:=&cat[[ &+[ (r)^i : r in rtsM_PHI | Evaluate(hi,r) eq 0 ] : i in [0..Degree(hi)-1] ] : hi in h_fac];
+//                 gens_E:=[ eps(g) : g in gens_E_inM ];
+//                 E:=sub< N | gens_E >; //sometimes it seems to crash...
+//                 resrefl:=(Ilog(p,q)) mod Ilog(p,#ResidueClassField(E))  eq 0;
+//                 Append(~refl_fields,E);
+//                 Append(~all_resrefl,resrefl);
+//             end if;
+//             ////////////////----Shimura-Taniyama----///////////////////
+//             st_tests:=[];
+//             for iP in [1..#primes] do
+//                 RHS_N:=#[ r : r in rtsM_PHI | Valuation(Evaluate(hp_fac[iP],eps(r))) gt (prec div 2) ];
+//                 LHS:=valsFL[iP]/valsq[iP];
+//                 RHS:=RHS_N/RHS_D_P[iP];
+//                 Append(~st_tests, LHS eq RHS );
+//             end for;
+//             st:=&and(st_tests);
+//             Append(~all_st,st);
+//         end for;
+//         RRC_CMTypes:=[ all_cm[i] : i in [1..#all_cm] | all_resrefl[i] and all_st[i] ];
+//     return RRC_CMTypes;
+// end intrinsic;
 
 /*
 // TESTS
@@ -519,32 +522,6 @@ end intrinsic;
         AVh;
         time #ResidualReflexCondition(AVh);
     end for;
-
-
-
-    AttachSpec("packages/AbVarFq/packages.spec");
-    Attach("packages/PolsAbVarFpCanLift/ResRefCond.m");
-    PP<x>:=PolynomialRing(Integers());
-    SetVerbose("ResRefCond",0);
-    polys:=[
-        //(x^4-5*x^3+15*x^2-25*x+25)*(x^4+5*x^3+15*x^2+25*x+25) //early exit on N. fast
-        x^6+3*x^4-10*x^3+15*x^2+125 // no. early exit on N. takes ~20 minutes
-        ];
-    for h in polys do
-        AVh:=IsogenyClass(h);
-        AVh;
-        _:=[ CMPosElt(PHI) : PHI in AllCMTypes(AVh) ];
-        SetProfile(true);
-        time #ResidualReflexCondition(AVh);
-        SetProfile(false);
-        G_new:=ProfileGraph();
-        SetProfile(true);
-        time #CCO_OLD(AVh);
-        SetProfile(false);
-        G_old:=ProfileGraph();
-    end for;
-    ProfilePrintByTotalTime(G_new);
-    ProfilePrintByTotalTime(G_old);
 
     // problematic polys.
     // trigger errors
@@ -576,7 +553,7 @@ end intrinsic;
     end for;
 
 
-    //triggering errors in ShimuraTaniyma
+    //triggering errors in ShimuraTaniyma fixed
     AttachSpec("packages/AbVarFq/packages.spec");
     Attach("packages/PolsAbVarFpCanLift/ResRefCond.m");
     PP<x>:=PolynomialRing(Integers());
@@ -586,7 +563,5 @@ end intrinsic;
     for PHI in all_cm do
         ShimuraTaniyama(Ih,PHI);
     end for;
-
-
 
 */
